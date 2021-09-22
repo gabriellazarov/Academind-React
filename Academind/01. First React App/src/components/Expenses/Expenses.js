f6371cb1dc1c6321fef86e1c8a8ee532a7787a8e
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-09-22 14:38:42 +0300

    Add files via upload

--- Academind/01. First React App/src/components/Expenses/Expenses.js
+++ Academind/01. First React App/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2020");
@@ -22,6 +23,7 @@ function Expenses(props) {
           selected={filterYear}
           onFilterChange={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredList} />
         <ExpensesList items={filteredList} />
       </Card>
     </div>

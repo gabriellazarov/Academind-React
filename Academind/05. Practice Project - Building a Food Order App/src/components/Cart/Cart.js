cd6d96601e7b136ec628109cd1ae5fcfaa03d142
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-09-28 22:00:27 +0300

    Add files via upload

---
+++ Academind/05. Practice Project - Building a Food Order App/src/components/Cart/Cart.js
@@ -0,0 +1,53 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+
+import styles from './Cart.module.css';
+
+const Cart = (props) => {
+  const ctx = useContext(CartContext);
+
+  const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+  const hasItems = ctx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    ctx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    ctx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartitems = (
+    <ul className={styles['cart-items']}>
+      {ctx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartitems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={styles.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
